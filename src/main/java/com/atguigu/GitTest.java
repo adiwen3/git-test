31eeb800cf4d4bb6fdd703e16971ee4853610e9c
adiwen <302592706@qq.com>
2021-10-15 08:04:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author adiwen
+ * @date 2021/10/15 7:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
