bbea6fa56428be82102d6a2c6b11c945eb002719
adiwen <302592706@qq.com>
2021-10-15 08:15:11 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,6 +6,8 @@ package com.atguigu;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git-刚创建文件名是红色-工作的文件未添加到暂存区");
+        System.out.println("hello git-未修改文件名是黑色-已添加多暂存区的文件未修改");
+        System.out.println("hello git-未修改文件名是蓝色-已添加多暂存区的文件被修改");
     }
 }
